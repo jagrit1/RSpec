85dad81c525b1b9c78ce95f47930fbb678243846
Academy09 <jgyawali@spartaglobal.com>
2015-11-18 15:57:58 +0000

    test driven development for adding and deleting phone numbers

--- person.rb
+++ person.rb
@@ -1,12 +1,13 @@
 require "date"
 class Person
 	attr_accessor :dob, :first_name, :surname
-	attr_reader  :emails
+	attr_reader  :emails, :phone_numbers
 	def initialize(fname, sname, dob)
 		@dob = Date.parse(dob)
 		@first_name = fname.capitalize
 		@surname = sname.capitalize
 		@emails = []
+		@phone_numbers = []
 
 	end
 	def fullname
